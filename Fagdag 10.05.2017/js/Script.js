36f9b0cb5bfb0d7e416ca29abbee5f04948445b7
Mariusweidem <Mariusweidem@hotmail.com>
2017-06-01 14:04:12 +0200

    Fagdag

---
+++ Fagdag 10.05.2017/js/Script.js
@@ -0,0 +1,8 @@
+
+//Dette er skripten til Submitten på side 3
+var result_3 = document.getElementById("oppskrift");
+
+document.getElementById("submit3").onclick = function() {
+    
+    result_3.innerHTML = "Tusen takk for ditt bidrag! Oppskriften må valideres før den evt kommer med på nettsiden.<br> Mvh Baksnakk";    
+}
